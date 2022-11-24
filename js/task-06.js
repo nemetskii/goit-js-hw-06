eb806d145ddbc38c21078a7c3566cdb0f1bbd3f1
Andrey Vorozhbit <andrey.vorozhbit23@gmail.com>
2022-11-24 16:39:29 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -7,9 +7,9 @@ inputEl.addEventListener("blur", onBlur);
 function onBlur(event) {
   const input = event.currentTarget;
   const inputLength = Number(input.dataset.length);
+  input.classList.remove("valid", "invalid");
   if (input.value.length === inputLength) {
     input.classList.add("valid");
-    input.classList.remove("invalid");
   } else {
     input.classList.add("invalid");
   }
